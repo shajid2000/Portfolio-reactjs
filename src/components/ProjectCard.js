ed8d9df1b304a30f88c61cc14e52bd949527110d
shajid2000 <shajid10032000@gmail.com>
2022-09-20 17:42:14 +0530

    Url

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,9 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, Url }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <a style={{color: "white"}} href="/"><div className="proj-imgbx">
+      <a style={{color: "white"}} href={Url}><div className="proj-imgbx">
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
